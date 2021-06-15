d0210fef5fe3c7fae2b457c5e9c8e4162ca9b95d
Julian Henao Henao <jhenaohenao@gmail.com>
2021-06-15 15:26:15 -0500

    Form view creation to create a post

--- app/Http/Controllers/Backend/PostController.php
+++ app/Http/Controllers/Backend/PostController.php
@@ -17,7 +17,7 @@ class PostController extends Controller
     {
         // $posts = Post::latest()->get();
         // return view('posts.index', compact($posts));
-        
+
         $posts = Post::all();
         return view ('posts.index', compact('posts'));
     }
@@ -29,7 +29,7 @@ class PostController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
